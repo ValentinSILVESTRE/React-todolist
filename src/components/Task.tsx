78dcafe5a62020d5b8985a1e41705276a7430f73
ValentinSILVESTRE <valentin.silvestre@hotmail.com>
2022-12-04 14:14:44 +0100

    Update de la deadline et priority

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Priority } from '../assets/models/priority.model';
 import { TaskModel } from '../assets/models/task.model';
 import { taskStatus } from '../assets/models/taskStatus.model';
 import '../assets/styles/Task.css';
@@ -10,11 +11,21 @@ interface ITaskProps {
 
 export default function Task(props: ITaskProps) {
 	const [editable, setEditable] = useState(false);
-	const [title, setTitle] = useState(props.task.title);
-	const [status, setStatus] = useState(props.task.status);
+	const [task, setTask] = useState(props.task);
 
 	const updateTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setTitle(e.target.value);
+		setTask({ ...task, title: e.target.value });
+	};
+
+	const updateDeadline = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setTask({ ...task, deadline: new Date(e.target.value) });
+	};
+
+	const updatePriority = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setTask({
+			...task,
+			priority: Priority[e.target.value as keyof typeof Priority],
+		});
 	};
 
 	const toggleEditable = () => {
@@ -37,38 +48,56 @@ export default function Task(props: ITaskProps) {
 	};
 
 	const toggleStatus = () => {
-		const newStatus =
-			status === taskStatus.done ? taskStatus.todo : taskStatus.done;
-		setStatus(newStatus);
+		const status =
+			task.status === taskStatus.done ? taskStatus.todo : taskStatus.done;
+		setTask({ ...task, status });
 	};
 
 	return (
 		<div
 			className="task d-flex justify-content-between row mx-0"
-			data-priority={props.task.priority}
+			data-priority={task.priority}
 		>
 			<div className="content d-flex col-5">
 				<div
 					className="status me-3 d-flex align-items-center"
 					onClick={toggleStatus}
 				>
-					{status === taskStatus.done && (
+					{task.status === taskStatus.done && (
 						<i className="bi bi-check-circle"></i>
 					)}
-					{status === taskStatus.todo && (
+					{task.status === taskStatus.todo && (
 						<i className="bi bi-circle"></i>
 					)}
 				</div>
+				{editable && (
+					<>
+						<label htmlFor="priority" className="my-auto">
+							Priority:
+						</label>
+						<select
+							name="priority"
+							id="priority"
+							className="form-control"
+							value={task.priority}
+							onChange={updatePriority}
+						>
+							<option value="low">Low</option>
+							<option value="medium">Medium</option>
+							<option value="high">High</option>
+						</select>
+					</>
+				)}
 
 				{!editable && (
 					<p className="m-0 task__title d-flex align-items-center">
-						{title}
+						{task.title}
 					</p>
 				)}
 				{editable && (
 					<input
 						type="text"
-						value={title}
+						value={task.title}
 						onChange={updateTitle}
 						className="task__titleInput"
 					/>
@@ -76,9 +105,22 @@ export default function Task(props: ITaskProps) {
 			</div>
 
 			<div className="date col-5">
-				<p className="d-flex align-items-center m-0">
-					{props.task.deadline.toDateString()}
-				</p>
+				{!editable && (
+					<p className="d-flex align-items-center m-0">
+						{task.deadline.toDateString()}
+					</p>
+				)}
+				{editable && (
+					<input
+						type="date"
+						className="form-control"
+						autoComplete="off"
+						min={new Date().toISOString().split('T')[0]}
+						value={task.deadline.toISOString().split('T')[0]}
+						onChange={updateDeadline}
+						required
+					/>
+				)}
 			</div>
 
 			<ul className="d-flex justify-content-end p-0 col-2">
